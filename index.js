bb53ac8e14176cea19f8cd83abaf890531777dd5
Protap Ghosh <protapg518@gmail.com>
2023-07-24 23:44:53 +0600

    hosting project

--- index.js
+++ index.js
@@ -1,17 +1,134 @@
-require("dotenv").config();
 const express = require("express");
-const port = process.env.PORT || 5000;
-const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
-// middleware
+const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrk8jch.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   },
+});
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+     await client.connect();
+
+     const collegeCollection = client.db("admissionDB").collection("colleges");
+     const admissionCollection = client
+        .db("admissionDB")
+        .collection("admissions");
+
+     app.get("/allColleges", async (req, res) => {
+        const colleges = await collegeCollection.find({}).toArray();
+        res.send(colleges);
+     });
+     app.get("/college/:id", async (req, res) => {
+        const id = req.params;
+        const query = { _id: new ObjectId(id) };
+        const college = await collegeCollection.findOne(query);
+        res.send(college);
+     });
+
+     app.post("/admissions", async (req, res) => {
+        const data = req.body;
+        // console.log(data);
+        const query = { _id: new ObjectId(data.collegeId) };
+        const existing = await admissionCollection.findOne(query);
+        if (existing) {
+           res.send("You are already admit");
+        }
+        const result = await admissionCollection.insertOne(data);
+        res.send(result);
+     });
+
+     app.get("/student/:name", async (req, res) => {
+        const name = req.params.name;
+        const query = { name: name };
+        const result = await admissionCollection.findOne(query);
+        res.send(result);
+     });
+
+     app.get("/myCollege/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+
+        const result = await collegeCollection.findOne(query);
+        res.send(result);
+     });
+
+     app.post("/addReview/:collegeName", async (req, res) => {
+        const collegeName = req.params.collegeName;
+        const newReview = req.body;
+        collegeCollection.findOneAndUpdate(
+           { name: collegeName },
+           { $push: { reviews: newReview } },
+           { new: true },
+           (err, updatedDocument) => {
+              if (err) {
+                 console.error("Error adding review:", err);
+                 return res
+                    .status(500)
+                    .send({ error: "Failed to add review." });
+              }
+              // Return the updated document (optional)
+              res.send(updatedDocument);
+           }
+        );
+     });
+
+     app.patch("/editProfile/:id", async (req, res) => {
+        const id = req.params.id;
+        const { name, email, subject, selectCollegeName } = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+           $set: {
+              name: name,
+              email: email,
+              subject: subject,
+              selectCollegeName: selectCollegeName,
+           },
+        };
+        const result = await admissionCollection.updateOne(filter, updateDoc);
+        res.send(result);
+     });
+
+     app.get("/searchCollege", async (req, res) => {
+        const clgName = req.query.name;
+
+        const query = { name: clgName };
+
+        const result = await collegeCollection.findOne(query);
+        res.send(result);
+     });
+
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log(
+         "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+   } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+   }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-   res.send("Innovative Academy is running...");
+   res.send("server is running");
 });
 
 app.listen(port, () => {
-   console.log(`Innovative Academy is running on ${port}`);
+   console.log(`Server running is ${port}`);
 });
